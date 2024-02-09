9574caea11cec6c917cfb02cd37050510a7f2f43
lunarhike <lunarhike@gmail.com>
2024-02-09 23:15:09 +0100

    deleted dotenv

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,4 @@
 import type { Config } from "drizzle-kit";
-import * as dotenv from "dotenv";
-dotenv.config({ path: ".env" });
 
 if (!process.env.POSTGRES_URL) {
   throw new Error("POSTGRES_URL is not defined.");
