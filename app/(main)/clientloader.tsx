ee9765ad337f3c5176c29ef04b69170a2df96f6d
lunarhike <lunarhike@gmail.com>
2024-02-25 18:40:56 +0100

    client supsense test

---
+++ app/(main)/clientloader.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const Clientloader = () => {
+  return <div>loading from the client</div>;
+};
+
+export default Clientloader;
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,12 +2,13 @@ export const dynamic = "force-dynamic";
 
 import TodoList from "@/modules/todos/components/todo-list";
 import { Suspense } from "react";
+import Clientloader from "./clientloader";
 
 export default async function Home() {
   return (
     <div>
       <div>Suspense Test</div>
-      <Suspense fallback="loading....">
+      <Suspense fallback={<Clientloader />}>
         <TodoList />
       </Suspense>
     </div>
