09b3f24d585162ca98d5e02e2d65c7d46f6c316f
lunarhike <lunarhike@gmail.com>
2024-02-26 17:40:20 +0100

    removed suspense test

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,5 +1,3 @@
-export const dynamic = "force-dynamic";
-
 import TodoList from "@/modules/todos/components/todo-list";
 import { Suspense } from "react";
 import Clientloader from "./clientloader";
@@ -7,7 +5,6 @@ import Clientloader from "./clientloader";
 export default async function Home() {
   return (
     <div>
-      <div>Suspense Test</div>
       <Suspense fallback={<Clientloader />}>
         <TodoList />
       </Suspense>
